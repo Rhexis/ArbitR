1fa957bffd55e2cf17db8df3f8652c877a83e1ab
Tyler Clement <tyler.clement@gmail.com>
2021-08-17 21:18:34 +0800

    combine core & base

--- ArbitR/ServiceRegistrar.cs
+++ ArbitR/ServiceRegistrar.cs
@@ -1,5 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using ArbitR.Core;
+using ArbitR.Core.Extensions;
 using ArbitR.Handlers;
 using ArbitR.Pipeline;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,7 +20,7 @@ namespace ArbitR
         public static void AddArbitR(this IServiceCollection services, params Assembly[] assemblies)
         {
             services.AddTransient<IArbiter, Arbiter>();
-            Core.ServiceRegistrar.Register
+            Register
             (
                 services,
                 assemblies.Distinct().ToArray(),
@@ -27,6 +31,47 @@ namespace ArbitR
                     typeof(IHandleQuery<,>),
                 }
             );
+        }
+        
+        private static void Register(IServiceCollection services, Assembly[] assemblies, IEnumerable<Type> handlers)
+        {
+            services.AddTransient<ServiceFactory>(p => p.GetService!);
+            
+            foreach (var handler in handlers)
+            {
+                services.GenerateTransientServices(handler, assemblies);
+            }
+        }
+
+        private static void GenerateTransientServices(this IServiceCollection services, Type handlerInterface, IEnumerable<Assembly> assemblies)
+        {
+            var concreteTypes = new List<Type>();
+            var interfaces = new List<Type>();
+            
+            foreach (TypeInfo typeInfo in assemblies.SelectMany(a => a.DefinedTypes).Where(t => !t.IsOpenGeneric()))
+            {
+                Type[] array = typeInfo.FindInterfaces(handlerInterface).ToArray();
+                if (!array.Any()) continue;
+                
+                if (typeInfo.IsConcrete())
+                {
+                    concreteTypes.Add(typeInfo);
+                }
+
+                foreach (Type type in array)
+                {
+                    if (interfaces.Contains(type)) continue;
+                    interfaces.Add(type);
+                }
+            }
+
+            foreach (Type type in interfaces)
+            {
+                foreach (Type implementation in concreteTypes.Where(x => x.CanBeCastTo(type)))
+                {
+                    services.AddTransient(type, implementation);
+                }
+            }
         }
     }
 }
